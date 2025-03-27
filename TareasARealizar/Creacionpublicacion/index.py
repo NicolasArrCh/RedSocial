cdab8046d8704b412208bdba69534f70ab4c31a5
Brayanmantilla <brayan21120409@gmail.com>
2025-03-27 16:43:09 -0500

    feat: :sparkles: Primera version de la creación de publicaciones

---
+++ TareasARealizar/Creacionpublicacion/index.py
@@ -0,0 +1,36 @@
+from class_firebase_database import FirebaseDB
+from firebase_admin import db
+import json
+
+path = r"E:\CAMPUSLAND\Software\4.Scrum\RedSocial\TareasARealizar\InicioDeSesiónDeUsuario\project_credentials.json"
+
+url ="https://tribucode-85a86-default-rtdb.firebaseio.com/"
+
+fb_db=FirebaseDB(path, url)
+
+ref=db.reference('users')
+new_id_user=ref.push()
+new_id_user.set({
+        "nickname":"Juanito123",
+        "nombre": "Juan",
+        "apellido": "Pérez",
+        "correo": "juan@example.com",
+        "fecha_nacimiento": "1990-05-12",
+        "genero": "Masculino",
+        "pais": "Colombia",
+        "telefono": "+573001234567",
+        "contraseña":"12345678"
+    })
+# fb_db.write_record('users/Juanito123', data_user)
+ref=db.reference('posts')
+new_id_post=ref.push()
+new_id_post.set({
+    "post_id": "P001",
+    "nickname": "Juanito123",
+    "contenido": "¡Hola, mundo!",
+    "fecha": "2025-03-26",
+    "likes":"0",
+    "comentarios":"0"
+})
+
+
